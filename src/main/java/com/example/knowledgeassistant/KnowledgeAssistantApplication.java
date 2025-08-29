c1945a2f9b26db2405bc6c5a102fc6690a618d4e
baikaishui <3259075994@qq.com>
2025-08-29 21:46:18 +0800

    初始代码

---
+++ src/main/java/com/example/knowledgeassistant/KnowledgeAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/knowledgeassistant/KnowledgeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
